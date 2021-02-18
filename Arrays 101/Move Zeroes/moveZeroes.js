107d0e6aab5ba22b95257d76f92945bc1b79a551
shahryar haghighi fard <shahryarhaghighifard@shahryars-MacBook-Pro.local>
2021-02-18 09:13:59 -0500

    move zeroes

---
+++ Arrays 101/Move Zeroes/moveZeroes.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    
+    let end = nums.length
+    let start = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 0) {
+            let j = i+1;
+            while(nums[j] == 0 && j < nums.length){
+                j++
+            }
+            if(j<nums.length){
+                let temp = nums[j]
+                nums[j] = nums[i]
+                nums[i] = temp
+            }else{
+                return nums
+            }
+            
+            
+        }
+        
+    }
+
+    return nums
+
+
+
+};
+
+console.log(moveZeroes([1,2,0,3,0,0,5,60,0]))
